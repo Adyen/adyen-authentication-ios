c9687511fac606e218bf5eb6612e2f838a8f69f5
mohamede <mohamed.eldoheiri@adyen.com>
2022-10-13 12:12:11 +0200

    adds support for macOS in SPM

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "AdyenAuthentication",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v10),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
